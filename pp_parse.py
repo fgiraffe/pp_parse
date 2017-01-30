f19f23b17e6fe0eebd6266cb64c85ab33d094ba4
Frank Giraffe <fgiraffe@gmail.com>
2017-01-29 20:15:22 -0800

    The inevitable typos.

--- pp_parse.py
+++ pp_parse.py
@@ -20,13 +20,13 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
+                                  ----
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
-    Why does this scrtipt exist?
+    Why does this script exist?
 
-    The Premeire Pro file format is officially undocumented.
+    The Premeiere Pro file format is officially undocumented.
     But AFAICT there is no easy way to just extract the files
     that a project references as TEXT. Exporting a batch list is a
     little ugly, and does not offer the full path.
