106f4de94350458814f7c98857d4b1f7340427fe
Frank Giraffe <fgiraffe@gmail.com>
2017-01-31 22:03:38 -0800

    passes flake8, closes test data file, better names.

--- tests/pp_parse_tests.py
+++ tests/pp_parse_tests.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
 
-from nose.tools import *
+from nose.tools import assert_equal
 from argparse import Namespace
 import json
 
 import pp_parse
 
-TEST_FILE_DATA = "tests/pp_parse_test_data.json"
+TEST_FILE_PATH = "tests/pp_parse_test_data.json"
+
+global test_file
+global test_file_data
+
 
 def setup():
-    print ("SETUP!")
+    global test_file
+    global test_file_data
+    with open(TEST_FILE_PATH, encoding='utf-8') as test_file:
+        test_file_data = json.loads(test_file.read())
+
 
 def teardown():
-    print ("TEAR DOWN!")
+    test_file.close()
 
-def test_basic():
-    print ("I RAN!")
 
 def test_one_file():
     args = Namespace(count=True)
-    file_list = pp_parse.print_media_paths("tests/PP_Tutorial_Lesson_05.prproj", args)
+    file_list = pp_parse.print_media_paths(
+                                    "tests/PP_Tutorial_Lesson_05.prproj", args)
     assert_equal(len(file_list), 33)
 
     args = Namespace()
     file_list = pp_parse.print_media_paths("tests/OneClip.prproj", args)
     assert_equal(len(file_list), 1)
 
+
 def test_all_files():
     args = Namespace()
-
-    with open(TEST_FILE_DATA, encoding='utf-8') as json_fp:
-        data = json.loads(json_fp.read())
-
-    for a_test_case in data:
+    for a_test_case in test_file_data:
         file_list = pp_parse.print_media_paths(a_test_case["filename"], args)
         assert_equal(len(file_list), a_test_case["UniqueFiles"])
